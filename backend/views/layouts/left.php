29a00d903c20a003faf47a77465cbb056146fe52
wlzx120 <wlzx120@163.com>
2017-09-15 16:15:40 +0800

    finish yii2-admin-menu

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -1,6 +1,8 @@
 <?php
+
 use yii\bootstrap\Nav;
 use mdm\admin\components\MenuHelper;
+use dmstr\widgets\Menu;
 
 ?>
 
@@ -70,45 +72,51 @@ use mdm\admin\components\MenuHelper;
             ]
         ) ?>
 
-        <ul class="sidebar-menu">
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-gears"></i> <span>栏目管理</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="/column"><i class="fa fa-circle-o"></i> 栏目列表</a></li>
-                    <li><a href="/column/create"><i class="fa fa-circle-o"></i> 栏目添加</a></li>
-                </ul>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-gears"></i> <span>权限控制</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li class="treeview">
-                        <a href="/admin">管理员</a>
-                        <ul class="treeview-menu">
-                            <li><a href="/user"><i class="fa fa-circle-o"></i> 后台用户</a></li>
-                            <li class="treeview">
-                                <a href="/admin/role">
-                                    <i class="fa fa-circle-o"></i> 权限 <i class="fa fa-angle-left pull-right"></i>
-                                </a>
-                                <ul class="treeview-menu">
-                                    <li><a href="/admin/route"><i class="fa fa-circle-o"></i> 路由</a></li>
-                                    <li><a href="/admin/permission"><i class="fa fa-circle-o"></i> 权限</a></li>
-                                    <li><a href="/admin/role"><i class="fa fa-circle-o"></i> 角色</a></li>
-                                    <li><a href="/admin/assignment"><i class="fa fa-circle-o"></i> 分配</a></li>
-                                    <li><a href="/admin/menu"><i class="fa fa-circle-o"></i> 菜单</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-        </ul>
+<!--        <ul class="sidebar-menu">-->
+<!--            <li class="treeview">-->
+<!--                <a href="#">-->
+<!--                    <i class="fa fa-gears"></i> <span>栏目管理</span>-->
+<!--                    <i class="fa fa-angle-left pull-right"></i>-->
+<!--                </a>-->
+<!--                <ul class="treeview-menu">-->
+<!--                    <li><a href="/column"><i class="fa fa-circle-o"></i> 栏目列表</a></li>-->
+<!--                    <li><a href="/column/create"><i class="fa fa-circle-o"></i> 栏目添加</a></li>-->
+<!--                </ul>-->
+<!--            </li>-->
+<!--            <li class="treeview">-->
+<!--                <a href="#">-->
+<!--                    <i class="fa fa-gears"></i> <span>权限控制</span>-->
+<!--                    <i class="fa fa-angle-left pull-right"></i>-->
+<!--                </a>-->
+<!--                <ul class="treeview-menu">-->
+<!--                    <li class="treeview">-->
+<!--                        <a href="/admin">管理员</a>-->
+<!--                        <ul class="treeview-menu">-->
+<!--                            <li><a href="/user"><i class="fa fa-circle-o"></i> 后台用户</a></li>-->
+<!--                            <li class="treeview">-->
+<!--                                <a href="/admin/role">-->
+<!--                                    <i class="fa fa-circle-o"></i> 权限 <i class="fa fa-angle-left pull-right"></i>-->
+<!--                                </a>-->
+<!--                                <ul class="treeview-menu">-->
+<!--                                    <li><a href="/admin/route"><i class="fa fa-circle-o"></i> 路由</a></li>-->
+<!--                                    <li><a href="/admin/permission"><i class="fa fa-circle-o"></i> 权限</a></li>-->
+<!--                                    <li><a href="/admin/role"><i class="fa fa-circle-o"></i> 角色</a></li>-->
+<!--                                    <li><a href="/admin/assignment"><i class="fa fa-circle-o"></i> 分配</a></li>-->
+<!--                                    <li><a href="/admin/menu"><i class="fa fa-circle-o"></i> 菜单</a></li>-->
+<!--                                </ul>-->
+<!--                            </li>-->
+<!--                        </ul>-->
+<!--                    </li>-->
+<!--                </ul>-->
+<!--            </li>-->
+<!--        </ul>-->
 
+        <?=
+            Menu::widget([
+                'options' => ['class' => 'sidebar-menu'],
+                'items' => MenuHelper::getAssignedMenu(Yii::$app->user->id)
+            ]);
+        ?>
     </section>
 
 </aside>
